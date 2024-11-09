f8af723e3258ebe8a8d2f63d623ac9f0a89d3838
wsk0715 <leindey@gmail.com>
2024-11-10 00:24:19 +0900

    design: modify Header layout component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,5 +3,5 @@ interface HeaderProps {
 }
 
 export default function Header({ title }: HeaderProps) {
-	return <div className="flex justify-center items-center gap-2 px-4 py-5 text-3xl">{title}</div>;
+	return <h1 className="text-3xl font-bold my-6 text-center">{title}</h1>;
 }
